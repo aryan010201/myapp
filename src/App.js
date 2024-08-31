d79c2796498be64196ced3586db2ebe2cf2b46be
aryan010201 <aryansharma010201@gmail.com>
2024-08-31 16:13:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './login.css';
+import Login from './login.js';
+import Student from './student';
+import './student.css';
+
+function App() {
+  return (
+   <Login />
+  );
+}
+
+export default App;
